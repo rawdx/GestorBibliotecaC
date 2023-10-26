2bb25554dc647782067d4be4f6812adfbfc0f368
DESKTOP-31VBQ8V\Puesto18 <raulcorderodiaz@gmail.com>
2023-10-26 14:33:40 +0200

    no message

---
+++ AccesoDatos/Contexto.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace AccesoDatos
+{
+    // Se define el contexto de conexión a base de datos, que después
+    // será instanciado como servicio para cada sesión https de usuario.
+    public class Contexto : DbContext
+    {
+        // Se define un constructor que nos permita generar el contexto
+        // como servicio desde el contenedor de servicios de la sesión https
+        // de usuario.
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+        // Aseguramos el uso de Ids autoincrementales.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+        // Los DbSet definen las operaciones básicas contra cada
+        // entidad de base de datos (CRUD).
+        // En los DbSet se definen colecciones de las clases definidas
+        // en Acceso.cs, lo que nos daría las entidades de la base de datos.
+        public DbSet<Acceso> Accesos { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<AutorLibro> Rel_Autores_Libros { get; set; }
+        public DbSet<Coleccion> Colecciones { get; set; }
+        public DbSet<Editorial> Editoriales { get; set; }
+        public DbSet<EstadoPrestamo> Estados_Prestamos { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<Prestamo> Prestamos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
---
+++ AccesoDatos/Entidades.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class Acceso
+    {
+        public long id_acceso {  get; set; }
+        public string codigo_acceso { get; set; }
+        public string descripcion_acceso { get; set; }
+    }
+    public class Autor
+    {
+        public long id_autor { get; set; }
+        public string nombre_autor { get; set; }
+        public string apellidos_autor { get; set; }
+    }
+    public class AutorLibro
+    {
+
+    }
+    public class Coleccion
+    {
+
+    }
+    public class Editorial
+    {
+
+    }
+    public class EstadoPrestamo
+    {
+
+    }
+    public class Genero
+    {
+
+    }
+    public class Libro
+    {
+
+    }
+    public class Prestamo
+    {
+
+    }
+    public class Usuario
+    {
+
+    }
+}
